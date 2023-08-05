524edcf99da41522d1d44d36f0d129f18aa663c7
htsuruo <tsuru.dev@gmail.com>
2023-08-05 16:35:00 +0900

    Add SelectionContainer.disabled to NavigationTitle

--- lib/widgets/navigation/navigation_title.dart
+++ lib/widgets/navigation/navigation_title.dart
@@ -8,14 +8,18 @@ class NavigationTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: () => context.go(routerInitialLocation),
-        child: Text(
-          'Flutter Admin Dashboard',
-          style: theme.textTheme.bodyLarge!.copyWith(
-            fontWeight: FontWeight.w700,
+    // SelectionAreaの影響でポインター表示にならないのでdisabledで例外対応
+    // ref. https://github.com/flutter/flutter/issues/104595#issuecomment-1378549493
+    return SelectionContainer.disabled(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () => context.go(routerInitialLocation),
+          child: Text(
+            'Flutter Admin Dashboard',
+            style: theme.textTheme.bodyLarge!.copyWith(
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
