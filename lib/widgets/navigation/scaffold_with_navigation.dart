1c8b29941ea2de3bd2844964a0b037ade9281351
htsuruo <tsuru.dev@gmail.com>
2023-08-05 20:37:11 +0900

    Add Drawer layout

--- lib/widgets/navigation/scaffold_with_navigation.dart
+++ lib/widgets/navigation/scaffold_with_navigation.dart
@@ -19,7 +19,7 @@ class ScaffoldWithNavigation extends StatelessWidget {
         builder: (context, constraints) {
           switch (constraints.maxWidth) {
             case < 450:
-              return _ScaffoldWithNavigationBar(navigationShell);
+              return _ScaffoldWithDrawer(navigationShell);
             default:
               return _ScaffoldWithNavigationRail(navigationShell);
           }
@@ -42,26 +42,9 @@ class _ScaffoldWithNavigationRail extends StatelessWidget {
       appBar: const NavigationAppBar(),
       body: Row(
         children: [
-          NavigationRail(
-            // extended: true,
-            selectedIndex: navigationShell.currentIndex,
-            unselectedLabelTextStyle: theme.textTheme.bodyMedium,
-            selectedLabelTextStyle: theme.textTheme.bodyMedium!.copyWith(
-              fontWeight: FontWeight.bold,
-            ),
-            onDestinationSelected: (index) {
-              navigationShell.goBranch(
-                index,
-                initialLocation: index == navigationShell.currentIndex,
-              );
-            },
-            destinations: [
-              for (final item in NavigationItem.values)
-                NavigationRailDestination(
-                  icon: Icon(item.iconData),
-                  label: Text(item.label),
-                ),
-            ],
+          _NavigationRail(
+            navigationShell: navigationShell,
+            expand: false,
           ),
           VerticalDivider(
             thickness: 1,
@@ -75,8 +58,8 @@ class _ScaffoldWithNavigationRail extends StatelessWidget {
   }
 }
 
-class _ScaffoldWithNavigationBar extends StatelessWidget {
-  const _ScaffoldWithNavigationBar(this.navigationShell);
+class _ScaffoldWithDrawer extends StatelessWidget {
+  const _ScaffoldWithDrawer(this.navigationShell);
 
   final StatefulNavigationShell navigationShell;
 
@@ -85,22 +68,75 @@ class _ScaffoldWithNavigationBar extends StatelessWidget {
     return Scaffold(
       appBar: const NavigationAppBar(),
       body: navigationShell,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: navigationShell.currentIndex,
-        onDestinationSelected: (index) {
-          navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
-          );
-        },
-        destinations: [
-          for (final item in NavigationItem.values)
-            NavigationDestination(
-              icon: Icon(item.iconData),
-              label: item.label,
-            ),
-        ],
+      drawer: Drawer(
+        child: _NavigationRail(
+          navigationShell: navigationShell,
+          expand: true,
+        ),
       ),
     );
   }
 }
+
+class _NavigationRail extends StatelessWidget {
+  const _NavigationRail({required this.navigationShell, required this.expand});
+
+  final StatefulNavigationShell navigationShell;
+  final bool expand;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return NavigationRail(
+      extended: expand,
+      selectedIndex: navigationShell.currentIndex,
+      unselectedLabelTextStyle: theme.textTheme.bodyMedium,
+      selectedLabelTextStyle: theme.textTheme.bodyMedium!.copyWith(
+        fontWeight: FontWeight.bold,
+      ),
+      onDestinationSelected: (index) {
+        navigationShell.goBranch(
+          index,
+          initialLocation: index == navigationShell.currentIndex,
+        );
+      },
+      destinations: [
+        for (final item in NavigationItem.values)
+          NavigationRailDestination(
+            icon: Icon(item.iconData),
+            label: Text(item.label),
+          ),
+      ],
+    );
+  }
+}
+
+// class _ScaffoldWithNavigationBar extends StatelessWidget {
+//   const _ScaffoldWithNavigationBar(this.navigationShell);
+
+//   final StatefulNavigationShell navigationShell;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: const NavigationAppBar(),
+//       body: navigationShell,
+//       bottomNavigationBar: NavigationBar(
+//         selectedIndex: navigationShell.currentIndex,
+//         onDestinationSelected: (index) {
+//           navigationShell.goBranch(
+//             index,
+//             initialLocation: index == navigationShell.currentIndex,
+//           );
+//         },
+//         destinations: [
+//           for (final item in NavigationItem.values)
+//             NavigationDestination(
+//               icon: Icon(item.iconData),
+//               label: item.label,
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
