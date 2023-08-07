cbdf0f5180a8b08763eaa08b0a82e41dd3451bb4
htsuruo <tsuru.dev@gmail.com>
2023-08-07 23:38:19 +0900

    Refactor import

--- lib/features/dashboard/dashbord_page.dart
+++ lib/features/dashboard/dashbord_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_admin_dashboard_template/widgets/summary_card.dart';
 import 'package:gap/gap.dart';
 import 'package:intersperse/intersperse.dart';
 import 'package:responsive_framework/responsive_framework.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'content_view.dart';
 export 'navigation/navigation.dart';
 export 'page_header.dart';
+export 'summary_card.dart';
