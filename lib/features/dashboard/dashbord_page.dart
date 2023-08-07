102bb5d88bc752bedf49eadf9a6c3085d01756d9
htsuruo <tsuru.dev@gmail.com>
2023-08-07 11:59:02 +0900

    Add dummy SummaryCard

--- lib/features/dashboard/dashbord_page.dart
+++ lib/features/dashboard/dashbord_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_admin_dashboard_template/widgets/summary_card.dart';
+import 'package:gap/gap.dart';
+import 'package:intersperse/intersperse.dart';
 
 import '../../widgets/widgets.dart';
 
@@ -7,8 +10,25 @@ class DashBoardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ContentView(
-      child: PageHeader(title: 'Dashboard'),
+    const summaryCards = [
+      SummaryCard(title: 'Total Sales', value: '\$125,000'),
+      SummaryCard(title: 'Total Users', value: '12,000'),
+      SummaryCard(title: 'KPI Progress Rate', value: '52.3%'),
+    ];
+    return ContentView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const PageHeader(title: 'Dashboard'),
+          const Gap(16),
+          Row(
+            children: summaryCards
+                .map<Widget>((card) => Expanded(child: card))
+                .intersperse(const Gap(16))
+                .toList(),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/summary_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SummaryCard extends StatelessWidget {
+  const SummaryCard({super.key, required this.title, required this.value});
+
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title, style: theme.textTheme.titleMedium),
+            Text(
+              value,
+              style: theme.textTheme.headlineMedium!.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
